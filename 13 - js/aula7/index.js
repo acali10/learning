cd5d0328a2200d2efc0e00e48d95350d6b27c342
Caline Nepomoceno <47435210+acali10@users.noreply.github.com>
2022-07-07 13:31:48 -0300

    .

---
+++ 13 - js/aula7/index.js
@@ -0,0 +1,18 @@
+let spaceship = prompt("Digite o nome da nave")
+
+let charToReplace = prompt("Qual caracter você deseja substituir?")
+
+let replacementChar = prompt("Por qual caracter você deseja subtituir?")
+
+let newSpaceship = ""
+
+for(let pos = 0; pos < spaceship.length; pos++){
+  if(spaceship[pos] == charToReplace) {
+    newSpaceship += replacementChar
+  } else {
+    newSpaceship += spaceship[pos]
+  }
+}
+
+alert(`O novo nome da nave é ${newSpaceship}`)
+
