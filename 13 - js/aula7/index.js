17e06dc862b3bdadfa7f3e73188e741c8ffaf997
Caline Nepomoceno <47435210+acali10@users.noreply.github.com>
2022-07-13 19:47:55 -0300

    ..

---
+++ 13 - js/aula7/index.js
@@ -0,0 +1,15 @@
+let spaceship = prompt("Digite o nome da nave")
+
+let charToReplace = prompt("Qual caracter você deseja substituir?")
+
+let replacementChar = prompt("Por qual caracter você deseja substituir?")
+
+let newSpaceship = ""
+
+for (let pos = 0; pos < spaceship.length; pos++){
+  if (spaceship[pos] == charToReplace){
+    newSpaceship += replacementChar
+  } else{
+    newSpaceship += spaceship[pos]
+  }
+}
