cec26efbf4ce4814b2e63b9568b35eaa15ee09d6
Caline Nepomoceno <caline.nepomoceno@outlook.com>
2024-03-11 20:59:55 -0300

    calculadora NÃO funcional

---
+++ javascript-modulo-1/exercicio-1/calculadora.js
@@ -0,0 +1,13 @@
+const entrada1 = prompt("Informe o primeiro número:")
+const entrada2 = prompt("Informe o segundo número:")
+
+const x = parseFloat(entrada1)
+const y = parseFloat(entrada2)
+
+const soma = x + y 
+const subtracao = x - y
+const multiplicacao = x * y
+const divisao = x / y 
+
+alert
+`Resultado: \n\n Some: ${soma}\n Subtração: ${subtracao} \nMultiplicação: ${multiplicacao}\n Divisão: ${divisao}`
